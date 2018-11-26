a7c5d0113b052972ac432e91abb00055b47306b4
arviZURCALED <45374862+arviZURCALED@users.noreply.github.com>
2018-11-27 07:58:33 +0800

    Create caesar_ciphe_Arvi dela cruzr.py

---
+++ caesar_ciphe_Arvi dela cruzr.py
@@ -0,0 +1,90 @@
+CCS 1A Programming 2
+CCS 1A
+Due Nov 20, 12:00 PM
+Challenge: Caesar Cipher
+
+Haisoj Nayahub
+Nov 20
+Using the pattern of Caesar Cipher, where A=B, B=C, C=D ....
+
+Write a python code capable of conversion, with dynamic input.
+
+Point System:
+
+10 pts for input
+20 pts for algorithm
+
+Total of: 30 pts
+This will serve as grade for Quiz. (LA)
+Your work
+Turned in
+
+dynamic_input_Arvi dela cruz.txt
+Text
+
+caesar_ciphe_Arvi dela cruzr.txt
+Text
+Challenge: Caesar Cipher
+name = input ("enter your name: ")
+
+name2 = ""
+
+for x in range (len(name)):
+    char = name [x]
+    
+    if char == "a":
+        name2 += "B"
+    elif char == "b":
+        name2 += "C"
+    elif char == "c":
+        name2 += "D"
+    elif char == "d":
+        name2 += "E"
+    elif char == "e":
+        name2 += "F"
+    elif char == "f":
+        name2 += "G"
+    elif char == "g":
+        name2 += "H"
+    elif char == "h":
+        name2 += "I"
+    elif char == "i":
+        name2 += "J"
+    elif char == "j":
+        name2 += "K"
+    elif char == "k":
+        name2 += "L"
+    elif char == "l":
+        name2 += "M"
+    elif char == "m":
+        name2 += "N"
+    elif char == "n":
+        name2 += "O"
+    elif char == "o":
+        name2 += "P"
+    elif char == "p":
+        name2 += "Q"
+    elif char == "q":
+        name2 += "R"
+    elif char == "r":
+        name2 += "S"
+    elif char == "s":
+        name2 += "T"
+    elif char == "t":
+        name2 += "U"
+    elif char == "u":
+        name2 += "V"
+    elif char == "v":
+        name2 += "W"
+    elif char == "w":
+        name2 += "X"
+    elif char == "x":
+        name2 += "Y"
+    elif char == "y":
+        name2 += "Z"
+    else:
+        name2 += char
+    
+print (name2)
+caesar_ciphe_Arvi dela cruzr.txt
+Displaying dynamic_input_Arvi dela cruz.txt.
