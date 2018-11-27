721bd3d8249982a9458afaf3c9903673b47d5500
arviZURCALED <45374862+arviZURCALED@users.noreply.github.com>
2018-11-27 08:03:22 +0800

    Create Sample_Arvi dela cruz.py

---
+++ Sample_Arvi dela cruz.py
@@ -0,0 +1,32 @@
+
+#string
+burger = "arvi"
+mango = "jerome"
+pinya = "lheo"
+
+#numeriacal value
+presyo = "100"
+
+#arithmetic
+addition = 10 + 10
+subtraction = 10 - 10
+multiplication = 10 * 10
+division = 10 / 10
+modulus = 10 %10
+
+print (addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(modulus)
+print(burger)
+print(mango)
+print(pinya)
+print(presyo)
+
+if addition > division:
+    print ("galingggggg")
+elif modulus >multiplication:
+    print ("vince payaat")
+else:
+    print ("vince bonak")
