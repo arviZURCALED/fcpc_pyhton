86fea4766e37077996bb969a044c27fc85d1d119
arviZURCALED <45374862+arviZURCALED@users.noreply.github.com>
2018-11-27 08:02:17 +0800

    Update dynamic_input_Arvi dela cruz.py

--- dynamic_input_Arvi dela cruz.py
+++ dynamic_input_Arvi dela cruz.py
@@ -1,30 +1,4 @@
-CCS 1A Programming 2
-CCS 1A
-Due Nov 20, 12:00 PM
-Challenge: Caesar Cipher
 
-Haisoj Nayahub
-Nov 20
-Using the pattern of Caesar Cipher, where A=B, B=C, C=D ....
-
-Write a python code capable of conversion, with dynamic input.
-
-Point System:
-
-10 pts for input
-20 pts for algorithm
-
-Total of: 30 pts
-This will serve as grade for Quiz. (LA)
-Your work
-Turned in
-
-dynamic_input_Arvi dela cruz.txt
-Text
-
-caesar_ciphe_Arvi dela cruzr.txt
-Text
-Challenge: Caesar Cipher
 name = input ("enter your name:")
 print ("your name is:", name)
 
@@ -34,5 +8,4 @@ lnum = int (input ("enter second number"))
 to_int_value = int ("100")
 print ("the sum is:" , vnum + lnum)
 print ("100 raise to 2:", to_int_value**2)
-dynamic_input_Arvi dela cruz.txt
-Displaying dynamic_input_Arvi dela cruz.txt.
+
